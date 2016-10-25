de71c0e75ea5195b5d7692428e39d1b5352ab46e
figroc <figroc@gmail.com>
2016-10-25 18:58:49 +0800

    refine

--- ca.sh
+++ ca.sh
@@ -14,8 +14,7 @@ echo FACE > ca/db/serial
 echo "00" > ca/crl/number
 
 openssl genrsa -out ca/ca.key 4096
-openssl req -config openssl.conf -key ca/ca.key \
-    -new -x509 -days 7300 -sha256 -extensions v3_ca \
-    -subj "/C=CN/ST=zhejiang/L=hangzhou/O=alibaba/OU=yunos/CN=TestEnvRootCA" \
+openssl req -config openssl.conf -extensions v3_ca -key ca/ca.key \
+    -new -x509 -days 7300 -sha256 -subj "/CN=TestEnvRootCA" \
     -out ca/ca.crt  
 openssl ca -config openssl.conf -gencrl -out ca/crl/ca.crl -crldays 30 
