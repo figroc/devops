bd95d7ec5f1b7e36f3101eb03d051e6fcb0ce08e
figroc <figroc@gmail.com>
2016-10-25 18:51:12 +0800

    cert tools: common config

---
+++ ca.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+echo "TestEnvRootCA: "
+read CONFIRM
+if ("$CONFIRM" != "NEW"); then
+    exit 1
+fi
+
+mkdir -p ca/certs
+mkdir -p ca/crl
+mkdir -p ca/db
+touch ca/db/index.txt
+touch ca/db/index.txt.attr
+echo FACE > ca/db/serial
+echo "00" > ca/crl/number
+
+openssl genrsa -out ca/ca.key 4096
+openssl req -config openssl.conf -key ca/ca.key \
+    -new -x509 -days 7300 -sha256 -extensions v3_ca \
+    -subj "/C=CN/ST=zhejiang/L=hangzhou/O=alibaba/OU=yunos/CN=TestEnvRootCA" \
+    -out ca/ca.crt  
+openssl ca -config openssl.conf -gencrl -out ca/crl/ca.crl -crldays 30 
