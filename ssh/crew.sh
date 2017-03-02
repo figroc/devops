841ce09f8fd21e4bc040e8ee83634c4c69aca580
figroc <figroc@gmail.com>
2017-03-02 19:27:20 +0800

    fix home dir group rights

--- ssh/crew.sh
+++ ssh/crew.sh
@@ -17,6 +17,7 @@ elif [ $# -eq 1 ]; then
     wget -O /var/gate/crews/$1.pub https://raw.githubusercontent.com/figroc/devops/master/pub/$1.pub
 elif [ $# -eq 2 ]; then
     adduser --home /home/jail/home/$1 --ingroup $1 --disabled-password --gecos '' --force-badname $1.$2
+    chmod -R g+rw /home/jail/home/$1
     usermod -a -G crews,jail $1.$2
     sed -i '/^'$1'\.'$2':.*/s@/home/'$1'\.'$2'@/home/jail/home/'$1'@' /etc/passwd
 fi
