094da3cce9a8116adc0ec664388ad9583bdc7cc3
figroc <figroc@gmail.com>
2017-03-04 02:39:22 +0800

    allow main crew account

--- ssh/crew.sh
+++ ssh/crew.sh
@@ -18,8 +18,11 @@ if [ $# -eq 0 ]; then
     mkdir -p ${gate}/servs
     mkdir -p ${gate}/crews
 elif [ $# -eq 1 ]; then
-    addgroup $1
+    adduser --home ${jail}/home/$1 --disabled-password --gecos '' $1
+    chmod -R g+rw ${jail}/home/$1
+    sed -i '/^'$1':.*/d' ${jail}/etc/passwd
     sed -i '/^'$1':.*/d' ${jail}/etc/group
+    grep '^'$1':' /etc/passwd | tee -a ${jail}/etc/passwd
     grep '^'$1':' /etc/group | tee -a ${jail}/etc/group
     wget -O ${gate}/crews/$1.pub https://raw.githubusercontent.com/figroc/devops/master/pub/$1.pub
 elif [ $# -eq 2 ]; then
@@ -27,15 +30,12 @@ elif [ $# -eq 2 ]; then
     sed -i '/^'$1'\.'$2':.*/s@:/home/'$1'\.'$2':@:/home/'$1':@' /etc/passwd
     sed -i '/^'$1'\.'$2':.*/d' ${jail}/etc/passwd
     grep '^'$1'\.'$2':' /etc/passwd | tee -a ${jail}/etc/passwd
-    chmod -R g+rw ${jail}/home/$1
     addgroup $2
-    usermod -a -G crews,jail,$2 $1.$2
+    usermod -a -G jail,crews,$2 $1.$2
     sed -i '/^jail:.*/d' ${jail}/etc/group
     sed -i '/^crews:.*/d' ${jail}/etc/group
-    sed -i '/^'$1':.*/d' ${jail}/etc/group
     sed -i '/^'$2':.*/d' ${jail}/etc/group
     grep '^jail:' /etc/group | tee -a ${jail}/etc/group
     grep '^crews:' /etc/group | tee -a ${jail}/etc/group
-    grep '^'$1':' /etc/group | tee -a ${jail}/etc/group
     grep '^'$2':' /etc/group | tee -a ${jail}/etc/group
 fi
