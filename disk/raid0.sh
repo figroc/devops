28b6508d2adbcd7ac6bf0f63e9a971affecf3ba0
figroc <figroc@gmail.com>
2017-03-07 15:57:11 +0800

    raid0 tool

---
+++ disk/raid0.sh
@@ -0,0 +1,32 @@
+if [ $# < 5 ]; then
+    echo 'usage: raid0.sh owner mnt-pt md-seq sd-seq ...'
+else
+    own=$1; shift
+    mnt=$1; shift
+    rmd=$1; shift
+    rno=$#; rsd=''
+    while (($#)); do
+        (   echo o  # clear partition
+            echo n  # new partition
+            echo p  # primary partition
+            echo 1  # partition number
+            echo    # default first sector 
+            echo    # default last sector
+            echo t  # change partition type
+            echo da # NoFS for raid
+            echo w  # write changes
+        ) | fdisk /dev/$1
+        rsd=${rsd}' /dev/'$1'1'
+        shift
+    done
+    if mdadm --create --verbos /dev/${rmd} --level=stripe --raid-devices=${rno} ${rsd}; then
+        mkfs.ext4 -v -m .1 -b 4096 /dev/${rmd}
+        mdadm --detail --scan | tee -a /etc/mdadm/mdadm.conf
+        uid=$(blkid /dev/${rmd} | grep -o 'UUID="[^"]+"')
+        echo ${uid//\"/}'   ext4    '${mnt}'    defaults    0   2' | tee -a /etc/fstab
+        mkdir -p ${mnt}
+        touch ${mnt}/WARNING
+        chown ${own}:${own} ${mnt}
+        mount -a
+    fi
+fi
