2c644b63ccde91ddc2c9e6035a6e0b6724aba99a
figroc <figroc@gmail.com>
2017-03-07 18:15:14 +0800

    typo fix

--- ssh/sshfs.sh
+++ ssh/sshfs.sh
@@ -8,17 +8,15 @@ gate='/etc/ssh/gate'
 case $1 in
     setup)
         sudo apt-get install sshfs
-        adduser devops fuse
         sed -i '/#user_allow_other[[:blank:]]*$/s/^#//' /etc/fuse.conf 
         ;;
 
     mount)
         mnt=$2
         if [ ! -z ${mnt} ]; then
-            mknod -m 666 /dev/fuse c 10 229
             mkdir -p ${mnt}
             chown devops:devops ${mnt}
-            sshfs -o allow_other,idmap=user,default_permissions,IdentityFile=${gate}/sys/agent.id \
+            sshfs -o allow_other,reconnect,IdentityFile=${gate}/sys/agent.id \
                 sftp@sftp: ${mnt} 
         fi
         ;;
@@ -26,7 +24,7 @@ case $1 in
     unmount)
         mnt=$2
         if [ ! -z ${mnt} ]; then
-            unmount ${mnt}
+            umount ${mnt}
         fi
         ;;
 
