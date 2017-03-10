346e641476347b110c87b7e6e30dc7e9d6a2c749
figroc <figroc@gmail.com>
2017-03-10 12:19:32 +0800

    enable docker to sudo

--- host/docker.sh
+++ host/docker.sh
@@ -7,8 +7,15 @@ user=$1
 home='/var/data/home'
 pubs='https://raw.githubusercontent.com/figroc/devops/master/pub'
 
+if [ -z /etc/sudoers.d/docker ]; then
+    mkdir -p /etc/sudoers.d
+    echo '%docker  ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/docker
+fi
+
 mkdir -p ${home}
-useradd -m -b ${home} -s /usr/sbin/nologin ${user}
-gpasswd -a ${user} docker
+if useradd -m -b ${home} -s /usr/sbin/nologin ${user}; then
+    gpasswd -a ${user} docker
+fi
+
 mkdir -p ${home}/${user}/.ssh
 wget -O ${home}/${user}/.ssh/authorized_keys ${pubs}/${user}.pub
