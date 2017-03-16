f05769c5d4af651bfc0a875b239bc17982afa761
figroc <figroc@gmail.com>
2017-03-16 11:11:26 +0800

    ellaborate sftpfs

--- disk/sftpfs.sh
+++ disk/sftpfs.sh
@@ -10,18 +10,26 @@ mnt='/var/sftp'
 case $1 in
     setup)
         apt-get install sshfs
-        sed -i '/#user_allow_other[[:blank:]]*$/s/^#//' /etc/fuse.conf 
+        sed -i '/#user_allow_other/s/#//' /etc/fuse.conf
+
+        mkdir -p ${mnt}
+        touch ${mnt}/WARNING
+        chown ${own}:${own} ${mnt}
+
+        sed -i '/sshfs sftp@sftp: .*/d' /etc/rc.local
+        sed -i '/exit 0/d' /etc/rc.local
+        echo "sshfs sftp@sftp: ${mnt} -o allow_other,reconnect,nonempty,"\
+             "IdentityFile=${gate}/sys/agent.id " >> /etc/rc.local
+        echo "exit 0" >> /etc/rc.local
         ;;
 
     mount)
-        mkdir -p ${mnt}
-        chown ${own}:${own} ${mnt}
-        sshfs -o allow_other,reconnect,IdentityFile=${gate}/sys/agent.id \
-            sftp@sftp: ${mnt} 
+        sshfs sftp@sftp: ${mnt} -o allow_other,reconnect,nonempty \
+              -o IdentityFile=${gate}/sys/agent.id
         ;;
 
     umount)
-        umount ${mnt}
+        fusermount -u ${mnt}
         ;;
 
     *)
