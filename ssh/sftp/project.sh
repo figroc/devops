d4629813196eb74bfc85e4c3427b462a5a4673f7
figroc <figroc@gmail.com>
2017-03-24 11:24:00 +0800

    add delivery dir

--- ssh/sftp/project.sh
+++ ssh/sftp/project.sh
@@ -13,6 +13,8 @@ chown -R root:root ${jail}/home/${proj}
 chmod -R o-r ${jail}/home/${proj}
 mkdir -p ${jail}/home/${proj}/data
 chown -R ${proj}:${proj} ${jail}/home/${proj}/data
+mkdir -p ${jail}/home/${proj}/data/delivery
+chown -R sftp:sftp ${jail}/home/${proj}/data/delivery
 mkdir -p ${jail}/data/projects
 cd ${jail}/data/projects && \
 ln -srf ../../home/${proj}/data ${proj}
