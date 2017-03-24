7c3c00d4306f82323813247c965c4f34b83fe0ac
figroc <figroc@gmail.com>
2017-03-24 18:16:43 +0800

    using bind instead of symlink

--- ssh/sftp/project.sh
+++ ssh/sftp/project.sh
@@ -15,6 +15,11 @@ mkdir -p ${jail}/home/${proj}/data
 chown -R ${proj}:${proj} ${jail}/home/${proj}/data
 mkdir -p ${jail}/home/${proj}/data/delivery
 chown -R sftp:sftp ${jail}/home/${proj}/data/delivery
-mkdir -p ${jail}/data/projects
-cd ${jail}/data/projects && \
-ln -srf ../../home/${proj}/data ${proj}
+
+mkdir -p ${jail}/data/projects/${proj}
+if mount --bind \
+    ${jail}/home/${proj}/data \
+    ${jail}/data/projects/${proj}; then
+    echo ${jail}/home/${proj}/data$'\t'${jail}/data/projects/${proj}$'\tnone\tbind\t0\t0' \
+    | tee -a /etc/fstab
+fi
