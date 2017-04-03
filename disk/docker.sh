e8d2f2c0fc9febb37b386b2f533b857bb2103c2c
figroc <figroc@gmail.com>
2017-04-04 03:28:25 +0800

    add docker storage setup script

---
+++ disk/docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# docker storage setup
+#
+
+mnt='/var/data/mnt/docker'
+
+systemctl stop docker
+if mkdir -p ${mnt}; then
+    chown root:root ${mnt}
+    chmod go-rw ${mnt}
+fi
+(   echo '{'
+    echo '  "graph": "'${mnt}'",'
+    echo '  "live-restore": true,'
+    echo '  "registry-mirrors":'
+    echo '      ["https://f62945bb.mirror.aliyuncs.com"]'
+    echo '  }'
+    echo '}'
+) | tee /etc/docker/daemon.json
+systemctl start docker
--- disk/raid0.sh
+++ disk/raid0.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+#
+# raid0 disk setup
+#
 
 own='devops'
 mnt='/var/data'
