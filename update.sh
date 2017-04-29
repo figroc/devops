de532ee01135b76f668fb1fbadb97728b79fb4cd
figroc <figroc@gmail.com>
2017-04-29 11:29:39 +0800

    more aggressive update

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-ssh $1 "sudo apt-get update"
-ssh $1 "sudo apt-get -y upgrade"
-ssh $1 "git -C devops pull"
+sudo apt-get -y update && \
+sudo apt-get -y upgrade && \
+sudo apt-get -y dist-upgrade && \
+sudo apt-get -y autoremove && \
+git -C devops pull
