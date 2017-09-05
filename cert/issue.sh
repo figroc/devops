07059146e976df9157da6c088dddeb28129d990c
figroc <figroc@gmail.com>
2017-09-05 16:45:53 +0800

    issue.sh feature: sign

--- cert/issue.sh
+++ cert/issue.sh
@@ -4,34 +4,29 @@ odir=${BASH_SOURCE%/*}
 conf=${odir}/openssl.conf
 ca_f=${odir}/ca/ca.crt
 
-function issue {
+function genr {
     subj_n=$3
     subj_d=${odir}/$1/$2
     subj_f=${subj_d}/$1
 
-    case $1 in
-        server)
-            ext_n='server_cert'
-            ;;
-        user)
-            ext_n='usr_cert'
-            ;;
-    esac
-
     mkdir -p ${subj_d}
+    if [ -f $1.csr ]; then
+        mv $1.csr ${subj_d}/
+    else
+        openssl genrsa -out ${subj_f}.key 4096
+        openssl req -config ${conf} -key ${subj_f}.key \
+            -new -sha256 -subj "/CN=${subj_n}" \
+            -out ${subj_f}.csr
+    fi
+}
 
+function sign {
     if [ -f ${subj_f}.crt ]; then
         openssl ca -config ${conf} -revoke ${subj_f}.crt
     fi
-
-    openssl genrsa -out ${subj_f}.key 4096 
-    openssl req -config ${conf} -key ${subj_f}.key \
-        -new -sha256 -subj "/CN=${subj_n}" \
-        -out ${subj_f}.csr  
     openssl ca -config ${conf} -extensions ${ext_n} \
         -days 375 -notext -md sha256 \
         -in ${subj_f}.csr -out ${subj_f}.crt
-
     openssl verify -CAfile ${ca_f} ${subj_f}.crt
     cat ${subj_f}.crt ${ca_f} > ${subj_f}.crt.chain
 }
@@ -48,10 +43,14 @@ fi
 
 case $1 in
     server)
-        issue server $2 *.$2
+        ext_n='server_cert'
+        genr server $2 *.$2
+        sign
         ;;
     user)
-        issue user $2 $2
+        ext_n='usr_cert'
+        genr user $2 $2
+        sign
         ;;
     *)
         usage
