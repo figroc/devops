4666ad7980586ba8c486d50e15160d6d25d2fc2f
figroc <figroc@gmail.com>
2017-10-11 15:55:52 +0800

    enable stream forwarding

---
+++ ssh/gateway/cmds/devel.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+
+function z_err() {
+    if [[ -z ${2} ]]; then echo ${1} 1>&2; exit 1; fi
+}
+
+function s_rm() {
+    if [[ -f ${1} ]]; then sudo rm ${1}; fi
+}
+
+read -a args <<< "${SSH_ORIGINAL_COMMAND}"
+
+usr=${1}; z_err "user not specified" ${usr};
+hot=${2}; z_err "host not specified" ${hot};
+pot=${3}; z_err "port not specified" ${pot};
+cmd=${args[0]};
+opt="";
+
+case ${cmd} in
+    jupyter)
+        s_rm /tmp/${user}.jupyter
+        opt="-L/tmp/${usr}.jupyter:127.0.0.1:8888"
+        ;;
+    *)
+        ;;
+esac
+
+ssh -q ${opt} -p${pot} ${usr}@${hot}
