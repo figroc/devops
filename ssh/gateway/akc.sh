11c8478a978c5ea7673018a681e756d84c9efff6
figroc <figroc@gmail.com>
2017-11-13 20:14:38 +0800

    honor crews first

--- ssh/gateway/akc.sh
+++ ssh/gateway/akc.sh
@@ -7,21 +7,21 @@
 #
 
 gate='/etc/ssh/gate'
-projs=("greenet" "s360" "wind" "sse" "gionee")
+projs="greenet s360 wind sse gionee"
 
 IFS=. read user role <<EOF
 ${1}
 EOF
 
-pub=${gate}/crews/${user}.pub
-opt=${gate}/roles/${role}.opt
-for pi in ${projs[@]}; do
-    if [[ ${pi} == ${role} ]]; then
-        pub=${gate}/projs/${role}/${user}.pub
-        opt=${gate}/roles/none.opt
-        break
-    fi
-done
+if id -nG ${user}.${role} | grep -qw crews; then
+    pub=${gate}/crews/${user}.pub
+    opt=${gate}/roles/${role}.opt
+elif echo ${projs} | grep -qw ${role}; then
+    pub=${gate}/projs/${role}/${user}.pub
+    opt=${gate}/roles/none.opt
+else
+    exit 1
+fi
 
 cmd=$(grep ${opt} \
     -e '^[[:blank:]]*[*][[:blank:]]*:[[:blank:]]*' \
