895b402eacfc8f901fc2022f36045b952e6deebe
figroc <figroc@gmail.com>
2017-11-14 10:29:28 +0800

    start nvidia-docker

--- devel/nvidia.sh
+++ devel/nvidia.sh
@@ -9,9 +9,10 @@ add-apt-repository multiverse
 add-apt-repository ppa:graphics-drivers
 apt-get update
 apt-get install -y nvidia-384 nvidia-modprobe mesa-common-dev
+shutdown -r now
 
 wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.1/nvidia-docker_1.0.1-1_amd64.deb
 dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb
+service nvidia-docker start
 
-shutdown -r now
 nvidia-docker run --rm nvidia/cuda nvidia-smi
