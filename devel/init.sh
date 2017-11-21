9a8dee011371138f293f673b17cb4f6f9065f218
figroc <figroc@gmail.com>
2017-11-21 16:38:29 +0800

    set vm map

--- devel/init.sh
+++ devel/init.sh
@@ -5,6 +5,11 @@
 
 source $(dirname ${0})/../env
 
+if [ ! -f /etc/sysctl.d/60-elastic.conf ]; then
+    echo "vm.max_map_count=262144" | sudo tee /etc/sysctl.d/60-elastic.conf
+    sudo sysctl -w vm.max_map_count=262144
+fi
+
 if ! grep deepro.io /etc/hosts; then
     echo "10.2.0.4  deepro.io" | sudo tee -a /etc/hosts
 fi
