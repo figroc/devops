6df561cbf9e2ca914f99f24e18783aa643740459
figroc <figroc@gmail.com>
2017-12-08 23:57:28 +0800

    create devops

--- init.sh
+++ init.sh
@@ -5,6 +5,16 @@
 
 source $(dirname ${0})/../env
 
+if [ "$(id -u)" -eq "0" ]; then
+    useradd -mU -c Ubuntu ${devops}
+    usermod -a ${devops} -G sudo,dialout,dip,plugdev,netdev,cdrom,floppy,audio,video
+    mkdir -p /home/${devops}/.ssh
+    cp .ssh/authorized_keys /home/${devops}/.ssh/
+    chown -R ${devops}:${devops} /home/${devops}/.ssh
+    chmod -R go-rwx /home/${devops}/.ssh
+    logout
+fi
+
 if [ ! -f /etc/sysctl.d/60-elastic.conf ]; then
     echo "vm.max_map_count=262144" | sudo tee /etc/sysctl.d/60-elastic.conf
     sudo sysctl -w vm.max_map_count=262144
