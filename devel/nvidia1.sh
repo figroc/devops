c15800a9dcbf7bada5f38b467798d7893937680a
figroc <figroc@gmail.com>
2018-01-09 16:49:40 +0800

    check fix

--- devel/nvidia1.sh
+++ devel/nvidia1.sh
@@ -5,7 +5,11 @@
 
 source $(dirname ${0})/../env
 
-if ! dpkg -l nvidia-384; then
+if dpkg -s nvidia-docker2 >/dev/null 2>&1; then
+  apt-get purge -y nvidia-docker2
+fi
+
+if ! dpkg -s nvidia-384 >/dev/null 2>&1; then
   add-apt-repository multiverse
   add-apt-repository ppa:graphics-drivers
   apt-get update && apt-get install -y \
@@ -15,7 +19,7 @@ if ! dpkg -l nvidia-384; then
   shutdown -r now
 fi
 
-if ! dpkg -l nvidia-docker; then
+if ! dpkg -s nvidia-docker >/dev/null 2>&1; then
   wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.1/nvidia-docker_1.0.1-1_amd64.deb
   dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb
   service nvidia-docker start
--- devel/nvidia2.sh
+++ devel/nvidia2.sh
@@ -5,14 +5,14 @@
 
 source $(dirname ${0})/../env
 
-if ! dpkg -l nvidia-docker >/dev/null 2>&1; then
+if dpkg -s nvidia-docker >/dev/null 2>&1; then
   docker volume ls -q -f driver=nvidia-docker | \
     xargs -r -I{} -n1 docker ps -q -a -f volume={} | \
     xargs -r docker rm -f
   apt-get purge -y nvidia-docker
 fi
 
-if ! dpkg -l cuda-drivers >/dev/null 2>&1; then
+if ! dpkg -s cuda-drivers >/dev/null 2>&1; then
   wget -P /tmp http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.1.85-1_amd64.deb
   dpkg -i /tmp/cuda-repo-*.deb && rm /tmp/cuda-repo-*.deb
   apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
@@ -21,7 +21,7 @@ if ! dpkg -l cuda-drivers >/dev/null 2>&1; then
   shutdown -r now
 fi
 
-if ! dpkg -l nvidia-docker2 >/dev/null 2>&1; then
+if ! dpkg -s nvidia-docker2 >/dev/null 2>&1; then
   curl -fsSL https://nvidia.github.io/nvidia-docker/gpgkey | apt-key add -
   wget -P /etc/apt/sources.list.d https://nvidia.github.io/nvidia-docker/ubuntu16.04/amd64/nvidia-docker.list
   apt-get update && echo "N" | apt-get install -y \
