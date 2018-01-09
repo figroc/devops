d16fd6b7c4f7ca04fa48f8b5cfc77ea186cb67ce
figroc <figroc@gmail.com>
2018-01-09 15:22:42 +0800

    une deps

--- devel/docker.sh
+++ devel/docker.sh
@@ -8,7 +8,6 @@ cert=$(dirname ${0})/../cert/server/asset.ca.crt
 
 apt-get update
 apt-get -y remove docker docker.io
-apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual
 apt-get -y install curl apt-transport-https ca-certificates software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
