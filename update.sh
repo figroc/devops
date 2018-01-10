bfb1162042bdd5eb85871472e6e8ece688dbd6ac
figroc <figroc@gmail.com>
2018-01-10 13:49:59 +0800

    hold git ci runner

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-sudo apt-mark hold docker-ce && \
+sudo apt-mark hold docker-ce gitlab-ci-multi-runner && \
 sudo apt-get -y update && \
 sudo apt-get -y upgrade && \
 sudo apt-get -y dist-upgrade && \
