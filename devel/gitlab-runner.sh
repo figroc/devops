8724459549cc89a1b9b7523a1c18015526295187
figroc <figroc@gmail.com>
2018-01-18 16:42:27 +0800

    mount host daemon.json

--- devel/gitlab-runner.sh
+++ devel/gitlab-runner.sh
@@ -10,6 +10,7 @@ while [[ -z "${R_TOKENS}" ]]; do read -p "Registration token: " R_TOKENS; done
 gitlab-runner register -n \
   -u ${R_SERVER} -t ${R_TOKENS} \
   --executor docker --docker-privileged --docker-image alpine \
+  --docker-volumes /etc/docker/daemon.json:/etc/docker/daemon.json:ro \
   --docker-volumes /etc/docker/certs.d:/etc/docker/certs.d:ro \
   --docker-volumes /var/file/inn:/var/file/inn:rw && \
 awk -i inplace \
