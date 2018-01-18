96d4481ddeef275b07a5d2cfc46fc6525af9c6bb
figroc <figroc@gmail.com>
2018-01-18 21:23:54 +0800

    gitlab runner register fix

--- devel/gitlab-runner.sh
+++ devel/gitlab-runner.sh
@@ -8,7 +8,7 @@ source $(dirname ${0})/../env
 while [[ -z "${R_SERVER}" ]]; do read -p "Gitlab coordinator: " R_SERVER; done
 while [[ -z "${R_TOKENS}" ]]; do read -p "Registration token: " R_TOKENS; done
 gitlab-runner register -n \
-  -u ${R_SERVER} -t ${R_TOKENS} \
+  -u ${R_SERVER} -r ${R_TOKENS} \
   --executor docker --docker-privileged --docker-image alpine \
   --docker-volumes /etc/docker/daemon.json:/etc/docker/daemon.json:ro \
   --docker-volumes /etc/docker/certs.d:/etc/docker/certs.d:ro \
