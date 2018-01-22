9cfd8c404de549d817bcd85b288334cd0874fcfe
figroc <figroc@gmail.com>
2018-01-23 00:20:11 +0800

    flag fix

--- azure/vm-mva.sh
+++ azure/vm-mva.sh
@@ -22,28 +22,31 @@ if echo "${avl}" | grep -qi "${ab}"; then
   exit 2
 fi
 
-vms=$(az_vm_show "hardwareProfile.vmSize")
-echo "Size: ${vms}"
+vms=$(az_vm_show "hardwareProfile.vmSize" | sed "s/_v2$//")
+echo "Size:  ${vms}"
+
+flg=""
 
 aod=$(az_vm_show "storageProfile.osDisk.managedDisk.id")
 if [[ -z "${aod}" ]]; then
   aod=$(az_vm_show "storageProfile.osDisk.vhd.uri")
+  flg="--use-unmanaged-disk"
 fi
-echo "OS Disk: ${aod}"
+echo "OS D:  ${aod}"
+
+nic=$(az_vm_show "networkProfile.networkInterfaces[].id")
+echo "NIC:   ${nic}"
+if [[ -n "${nic}" ]]; then
+  nic="--nics ${nic}"
+fi
 
 add=$(az_vm_show "storageProfile.dataDisks[].managedDisk.id")
 if [[ -z "${add}" ]]; then
   add=$(az_vm_show "storageProfile.dataDisks[].vhd.uri")
 fi
-echo "Data Disk: ${add}"
+echo "Data:  ${add}"
 if [[ -n "${add}" ]]; then
   add="--attach-data-disks ${add}"
-fi
-
-nic=$(az_vm_show "networkProfile.networkInterfaces[].id")
-echo "NIC: ${nic}"
-if [[ -n "${nic}" ]]; then
-  nic="--nics ${nic}"
 fi
 
 echo
@@ -51,6 +54,7 @@ echo "deleting..."
 az vm delete -g ${gp} -n ${vm} --verbose --yes
 echo
 echo "recreating..."
-az vm create -g ${gp} -n ${vm} --verbose --os-type linux \
-    --size ${vms} --availability-set ${ab} \
-    --attach-os-disk ${osd} ${add} ${nic}
+az vm create -g ${gp} -n ${vm} --verbose \
+    --os-type linux --size ${vms} \
+    --availability-set ${ab} ${flg} \
+    --attach-os-disk ${aod} ${add} ${nic}
