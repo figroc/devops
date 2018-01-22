dd0836c56fa201717f1dbea78f5abc46d5d3e073
figroc <figroc@gmail.com>
2018-01-23 00:47:34 +0800

    ~.~

--- devel/gitlab-runner.sh
+++ devel/gitlab-runner.sh
@@ -14,6 +14,6 @@ gitlab-runner register -n \
   --docker-volumes /etc/docker/daemon.json:/etc/docker/daemon.json:ro \
   --docker-volumes /etc/docker/certs.d:/etc/docker/certs.d:ro \
   --docker-volumes /var/file/inn:/var/file/inn:rw && \
--awk -i inplace \
--  'FNR==NR{if(/^\s+executor\s+=\s+"docker"$/)p=NR;next}1;FNR==p{print"  environment = [\"DOCKER_DRIVER=overlay2\"]"}' \
--  /etc/gitlab-runner/config.toml /etc/gitlab-runner/config.toml
+awk -i inplace \
+  'FNR==NR{if(/^\s+executor\s+=\s+"docker"$/)p=NR;next}1;FNR==p{print"  environment = [\"DOCKER_DRIVER=overlay2\"]"}' \
+  /etc/gitlab-runner/config.toml /etc/gitlab-runner/config.toml
