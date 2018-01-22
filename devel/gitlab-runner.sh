bccdd693e8b041951196e98219ea482d9f942666
figroc <figroc@gmail.com>
2018-01-22 23:13:49 +0800

    refine gitlab runner register

--- devel/gitlab-runner.sh
+++ devel/gitlab-runner.sh
@@ -8,11 +8,11 @@ source $(dirname ${0})/../env
 while [[ -z "${R_SERVER}" ]]; do read -p "Gitlab coordinator: " R_SERVER; done
 while [[ -z "${R_TOKENS}" ]]; do read -p "Registration token: " R_TOKENS; done
 gitlab-runner register -n \
+  --limit 1 --request-concurrency 1 \
   -u ${R_SERVER} -r ${R_TOKENS} \
-  --executor docker --docker-privileged --docker-image alpine \
+  --executor docker --docker-privileged \
+  --environment "DOCKER_DRIVER=overlay2" \
+  --docker-image alpine \
   --docker-volumes /etc/docker/daemon.json:/etc/docker/daemon.json:ro \
   --docker-volumes /etc/docker/certs.d:/etc/docker/certs.d:ro \
   --docker-volumes /var/file/inn:/var/file/inn:rw && \
-awk -i inplace \
-  'FNR==NR{if(/^\s+executor\s+=\s+"docker"$/)p=NR;next}1;FNR==p{print"  environment = [\"DOCKER_DRIVER=overlay2\"]"}' \
-  /etc/gitlab-runner/config.toml /etc/gitlab-runner/config.toml
