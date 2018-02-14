9d79058719b6cad9f21ce24bc9e4adbf3e6cefad
figroc <figroc@gmail.com>
2018-02-14 15:29:20 +0800

    make secondary addr permanent

--- network/ip.sh
+++ network/ip.sh
@@ -8,7 +8,11 @@ source $(dirname ${0})/../env
 act="${1}"
 case ${act} in
     add)# e.g. 10.0.0.1/24
-        ip addr add "${2}" dev eth0
+        ip address add "${2}" dev eth0
+        (
+          echo "auto eth0:1"
+          echo "iface eth0:1 inet dhcp"
+        ) >> /etc/network/interfaces.d/50-cloud-init.cfg
         ;;
     *)
         ;;
