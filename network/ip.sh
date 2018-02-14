bc03e752387ec8470941b60106a5d2a911c6aebc
figroc <figroc@gmail.com>
2018-02-14 15:56:00 +0800

    using ifup

--- network/ip.sh
+++ network/ip.sh
@@ -5,14 +5,22 @@
 
 source $(dirname ${0})/../env
 
+ifc="/etc/network/interfaces.d/50-cloud-init.cfg"
+
 act="${1}"
 case ${act} in
     add)# e.g. 10.0.0.1/24
-        ip address add "${2}" dev eth0
-        (
+        if grep eth0:1 ${ifc}; then
+            echo $'\n'"already exists" >&2
+            exit 1
+        fi
+        ( echo
           echo "auto eth0:1"
-          echo "iface eth0:1 inet dhcp"
-        ) >> /etc/network/interfaces.d/50-cloud-init.cfg
+          echo "iface eth0:1 inet static"
+          echo "address ${2}"
+          echo "netmask ${3}"
+        ) >> ${ifc}
+        ifup eth0:1
         ;;
     *)
         ;;
