31f8a47f6a4ddee0c37c450f6fafb64a37552641
figroc <figroc@gmail.com>
2018-02-24 13:46:34 +0800

    reverse bind mount

--- user/sftp.sh
+++ user/sftp.sh
@@ -18,15 +18,18 @@ chmod -R o-rw ${jail}/home/${proj}
 chmod -R g-w ${jail}/home/${proj}
 
 mkdir -p ${jail}/home/${proj}/data
-mkdir -p ${jail}/home/${proj}/data/delivery
 chown -R ${proj}:${proj} ${jail}/home/${proj}/data
-chown -R sftp:sftp ${jail}/home/${proj}/data/delivery
 chmod -R g+w ${jail}/home/${proj}/data
 chmod -R a+r ${jail}/home/${proj}/data
 
-mkdir -p ${jail}/data/projects/${proj}
-if mount --bind ${jail}/home/${proj}/data ${jail}/data/projects/${proj}; then
-    echo \
-        ${jail}/home/${proj}/data$'\t'${jail}/data/projects/${proj}$'\tnone\tbind\t0\t0' \
+mkdir -p ${jail}/data/external/${proj}
+chown -R ${proj}:${proj} ${jail}/data/external/${proj}
+mkdir -p ${jail}/data/external/${proj}/delivery
+chown -R sftp:sftp ${jail}/data/external/${proj}/delivery
+chmod -R g+w ${jail}/data/external/${proj}
+chmod -R a+r ${jail}/data/external/${proj}
+
+if mount --bind ${jail}/data/external/${proj} ${jail}/home/${proj}/data; then
+    echo ${jail}/data/external/${proj}$'\t'${jail}/home/${proj}/data$'\tnone\tbind\t0\t0' \
         | tee -a /etc/fstab
 fi
