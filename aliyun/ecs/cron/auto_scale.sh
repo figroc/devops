c29ec852a6ea57c659ea0e87e6ada40b8117189a
figroc <figroc@gmail.com>
2018-03-08 20:38:07 +0800

    array fix

--- aliyun/ecs/cron/auto_scale.sh
+++ aliyun/ecs/cron/auto_scale.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -e
 
-ecs=${1}
+source ${1}
 
 mon="$(dirname ${0})/../monitor.sh"
-if [[ "$(${mon} ${ecs})" == "idle" ]]; then
-    $(dirname ${0})/../stop.sh ${ecs}
-fi
+for ecs in ${SPOOL[@]}; do
+    if [[ "$(${mon} ${ecs})" == "idle" ]]; then
+        $(dirname ${0})/../stop.sh ${ecs}
+    fi
+done
--- aliyun/ecs/cron/install.sh
+++ aliyun/ecs/cron/install.sh
@@ -1,5 +1,3 @@
 #!/bin/bash -e
 
-ecs=${1}
-
-echo "10 * * * * devops /home/devops/devops/aliyun/ecs/cron/auto_scale.sh ${ecs}" > /etc/cron.d/auto_scale_vm
+echo "10 * * * * devops /home/devops/devops/aliyun/ecs/cron/auto_scale.sh /home/devops/devops/ssh/gateway/cmds/env" > /etc/cron.d/auto_scale_vm
