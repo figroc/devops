a8a9dd820fabbf323c19555971fe341ddec14589
figroc <figroc@gmail.com>
2018-03-29 13:24:33 +0800

    firewall

--- devel/tunnel.sh
+++ devel/tunnel.sh
@@ -5,21 +5,30 @@
 
 source $(dirname ${0})/../.env
 
-cat >/etc/systemd/system/stunnel.service <<EOF
+cat >/etc/systemd/system/stunnel.service <<'EOF'
 [Unit]
 Description=Secure Tunnel
 After=network.target
 
 [Service]
-RestartSec=5
 Restart=always
-User=devops
-Environment="TARGET=w.cloudbrain.pro"
-ExecStart=/usr/bin/ssh -NT -oServerAliveInterval=60 -oExitOnForwardFailure=yes -R2222:localhost:22 ${TARGET}
+RestartSec=37
+User=root
+ExecStart=/usr/bin/ssh -NT \
+    -i/home/devops/.ssh/id_rsa \
+    -oServerAliveInterval=60 \
+    -oExitOnForwardFailure=yes \
+    -R2222:localhost:22 \
+    -L0.0.0.0:443:deepro.io:443 \
+    devops@w.cloudbrain.pro
 
 [Install]
 WantedBy=multi-user.target
 EOF
+
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -i docker0 -j ACCEPT
+iptables -A INPUT -j REJECT
 
 systemctl enable stunnel.service
 systemctl start  stunnel.service
