21cf022319027af161f44d515bcdcfb3e9f83907
figroc <figroc@gmail.com>
2018-04-16 18:24:43 +0800

    dante proxy

---
+++ devel/dante.sh
@@ -0,0 +1,65 @@
+#!/bin/bash -e
+#
+# reverse tunnel setup
+#
+
+source $(dirname ${0})/../.env
+
+if [[ -z "${1}" ]]; then
+    echo "${0} <ext-eth-if>" 1>&2
+    exit 1
+fi
+DANTE_IF="${1}"
+
+apt-get install -y gcc libwrap0 libwrap0-dev make
+
+DANTE_HOME="/opt/dante"
+mkdir -p ${DANTE_HOME} && cd ${DANTE_HOME}
+
+DANTE_VERSION="dante-1.4.2"
+wget https://www.inet.no/dante/files/${DANTE_VERSION}.tar.gz
+tar -xvf ${DANTE_VERSION}.tar.gz && cd ${DANTE_VERSION}
+
+./configure --prefix=/opt/dante
+make
+make install
+
+cd .. && rm -rf ${DANTE_VERSION}
+
+adduser --system --disabled-login --no-create-home dante || true
+
+cat >danted.conf <<EOF
+logoutput: /var/log/danted.log
+internal: 127.0.0.1 port = 1080
+external: ${DANTE_IF}
+external.rotation: same-same
+user.notprivileged: dante
+clientmethod: none
+socksmethod: none
+client pass {
+  from: 0.0.0.0/0 to: 0.0.0.0/0
+  log: error # connect disconnect
+}
+socks pass {
+  from: 0.0.0.0/0 to: 0.0.0.0/0
+  log: error # connect disconnect iooperation
+}
+EOF
+
+cat >/etc/systemd/system/danted.service <<EOF
+[Unit]
+Description=Socks Proxy
+After=network.target
+
+[Service]
+Restart=always
+RestartSec=37
+User=socks
+ExecStart=sockd -f /opt/dante/danted.conf -D
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable danted.service
+systemctl start  danted.service
--- devel/tunnel.sh
+++ devel/tunnel.sh
@@ -5,41 +5,58 @@
 
 source $(dirname ${0})/../.env
 
-cat >/etc/systemd/system/stunnel.service <<'EOF'
+if [[ -z "${1}" ]]; then
+    echo "${0} <gateway> [port]" 1>&2
+    exit 1
+else
+    svc_target="devops@${1}"
+fi
+
+if [[ -z "${2}" ]]; then
+    svc_name="stunnel.service"
+    svc_title="Secure Tunnel"
+    svc_mappt="-R2222:localhost:22 -L0.0.0.0:443:deepro.io:443"
+else
+    svc_name="dp_${2}.service"
+    svc_title="Secure Proxy ${2}"
+    svc_mappt="-R0.0.0.0:${2}:localhost:${2}"
+fi
+
+cat >/etc/systemd/system/${svc_name} <<EOF
 [Unit]
-Description=Secure Tunnel
+Description=${svc_title}
 After=network.target
 
 [Service]
 Restart=always
 RestartSec=37
 User=root
-ExecStart=/usr/bin/ssh -NT \
-    -i/home/devops/.ssh/id_rsa \
-    -oServerAliveInterval=60 \
-    -oExitOnForwardFailure=yes \
-    -R2222:localhost:22 \
-    -L0.0.0.0:443:deepro.io:443 \
-    devops@w.cloudbrain.pro
+ExecStart=/usr/bin/ssh -NT \\
+    -i/home/devops/.ssh/id_rsa \\
+    -oServerAliveInterval=60 \\
+    -oExitOnForwardFailure=yes \\
+    ${svc_mappt} ${svc_target}
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
-iptable="/etc/network/if-pre-up.d/stunnel"
-cat >${iptable} <<'EOF'
-#!/bin/bash
-function iptable_rule_set {
-  iptables -C ${1} &>/dev/null || iptables -A ${1}
-}
-iptable_rule_set "INPUT -j ACCEPT -i lo"
-iptable_rule_set "INPUT -j ACCEPT -i docker0"
-iptable_rule_set "INPUT -j ACCEPT -p tcp --dport ssh"
-iptable_rule_set "INPUT -j ACCEPT -m state --state ESTABLISHED,RELATED"
-iptable_rule_set "INPUT -j REJECT"
-EOF
-chmod +x ${iptable}
-${iptable}
+if [[ "${svc_name}" == "" ]]; then
+    svc_iptable="/etc/network/if-pre-up.d/stunnel"
+    cat >${svc_iptable} <<-'EOF'
+	#!/bin/bash
+	function iptable_rule_set {
+	  iptables -C ${1} &>/dev/null || iptables -A ${1}
+	}
+	iptable_rule_set "INPUT -j ACCEPT -i lo"
+	iptable_rule_set "INPUT -j ACCEPT -i docker0"
+	iptable_rule_set "INPUT -j ACCEPT -p tcp --dport ssh"
+	iptable_rule_set "INPUT -j ACCEPT -m state --state ESTABLISHED,RELATED"
+	iptable_rule_set "INPUT -j REJECT"
+	EOF
+    chmod +x ${svc_iptable}
+    ${svc_iptable}
+fi
 
-systemctl enable stunnel.service
-systemctl start  stunnel.service
+systemctl enable ${svc_name}
+systemctl start  ${svc_name}
