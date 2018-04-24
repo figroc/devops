17af0d250b9cc0b2392fae7d1474fd831784f4cf
figroc <figroc@gmail.com>
2018-04-24 11:32:36 +0800

    refine update

--- update.sh
+++ update.sh
@@ -1,7 +1,22 @@
-#!/bin/bash -e
+#!/bin/bash
 
-git -C ~/devops checkout master && git -C ~/devops pull && \
-sudo apt-get -y update && \
-sudo apt-get -y upgrade && \
-sudo apt-get -y dist-upgrade && \
+g='git -C ~/devops'
+b=$(${g} branch | grep '^\* ' | cut -d' ' -f2)
+if [[ "${b}" != "master" ]]; then
+    ${g} checkout master
+fi
+${g} pull --rebase
+if [[ "${b}" != "master" ]]; then
+    ${g} checkout ${b}
+    ${g} rebase
+fi
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
 sudo apt-get -y autoremove
+
+while ((${#} > 0)); do
+    ssh -q ${1} ~/devops/update.sh
+    shift
+done
