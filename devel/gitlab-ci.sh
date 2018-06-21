4811a7dcf0a846a760c4e14556a13dfb0ad860c6
figroc <figroc@gmail.com>
2018-06-21 08:59:18 +0800

    curl fix

--- devel/gitlab-ci.sh
+++ devel/gitlab-ci.sh
@@ -5,7 +5,7 @@
 
 source $(dirname ${0})/../.env
 
-curl -fsSL https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh | bash
+curl -fSL https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh | bash
 cat > /etc/apt/preferences.d/pin-gitlab-runner.pref <<EOF
 Explanation: Prefer GitLab provided packages over the Debian native ones
 Package: gitlab-runner
