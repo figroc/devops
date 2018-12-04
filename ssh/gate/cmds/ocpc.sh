20d6dd0df1401cce7debbdbc48d34061cb7c728d
figroc <figroc@gmail.com>
2018-12-04 16:34:59 +0800

    reverse socks

--- ssh/gate/cmds/ocpc.sh
+++ ssh/gate/cmds/ocpc.sh
@@ -4,6 +4,12 @@ source $(dirname $0)/.env
 
 read -a args <<< "${SSH_ORIGINAL_COMMAND}"
 
-usr=${args[0]}; z_err "user not specified" ${usr};
+usr=${1}
+act=${args[0]}; z_err "user not specified" ${act};
 
-ssh -t -i /etc/ssh/gate/sys/agent.id -q jumper@pts.n.cloudbrain.pro ssh -p 2222 ${usr}@localhost
+rm -rf /tmp/${usr}.socks
+ssh -t -i /etc/ssh/gate/sys/agent.id \
+    -L/tmp/${usr}.socks:127.0.0.1:2080 \
+    -q jumper@pts.n.cloudbrain.pro \
+    ssh -p 2222 ${act}@localhost
+rm -rf /tmp/${usr}.socks
