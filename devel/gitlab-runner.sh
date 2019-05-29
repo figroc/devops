65a4f3764e3540e9840e1421d4b2390c87da9ebe
figroc <figroc@gmail.com>
2019-05-29 14:50:42 +0800

    docker in docker

--- devel/gitlab-runner.sh
+++ devel/gitlab-runner.sh
@@ -8,14 +8,13 @@ source $(dirname ${0})/../.env
 while [[ -z "${R_SERVER}" ]]; do read -p "Gitlab coordinator: " R_SERVER; done
 while [[ -z "${R_TOKENS}" ]]; do read -p "Registration token: " R_TOKENS; done
 gitlab-runner register -n --locked false \
-  --limit 1 --request-concurrency 1 \
   -u ${R_SERVER} -r ${R_TOKENS} \
   --executor docker --docker-privileged \
   --docker-image alpine \
-  --docker-services docker:dind \
   --env "DOCKER_DRIVER=overlay2" \
-  --env "DOCKER_HOST=tcp://docker:2375" \
   --env "GIT_SUBMODULE_STRATEGY=recursive" \
+  --docker-volumes "/var/data/docker" \
   --docker-volumes "/etc/docker/daemon.json:/etc/docker/daemon.json:ro" \
   --docker-volumes "/var/file/inn:/var/file/inn:rw" \
-  --docker-helper-image deepro.io/gitlab/gitlab-runner-helper
+  --docker-helper-image deepro.io/gitlab/gitlab-runner-helper \
+  --pre-build-script "service docker start"
