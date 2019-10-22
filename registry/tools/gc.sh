26a25d80d91fbf683a741be4159cdcc132a5f780
Figroc Chen <figroc@gmail.com>
2019-10-22 19:23:05 +0800

    cleanup

--- registry/tools/gc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-for c in $(docker ps -a | grep _registry_ | awk '{print $(1)}'); do
-    docker exec -it ${c%$'\r'} \
-        bin/registry garbage-collect \
-        /etc/docker/registry/config.yml
-done
