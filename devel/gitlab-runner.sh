fae2147d5a962c8e08be8e0d824c1e6b761ee5c5
Figroc Chen <figroc@gmail.com>
2019-12-17 18:47:57 +0800

    disable docker cache

--- devel/gitlab-runner.sh
+++ devel/gitlab-runner.sh
@@ -9,8 +9,10 @@ while [[ -z "${R_SERVER}" ]]; do read -p "Gitlab coordinator: " R_SERVER; done
 while [[ -z "${R_TOKENS}" ]]; do read -p "Registration token: " R_TOKENS; done
 gitlab-runner register -n --locked false \
   -u ${R_SERVER} -r ${R_TOKENS} \
-  --executor docker --docker-privileged \
+  --executor docker \
+  --docker-privileged \
   --docker-image alpine \
+  --docker-disable-cache \
   --env "GIT_SUBMODULE_STRATEGY=recursive" \
   --docker-helper-image deepro.io/gitlab/gitlab-runner-helper \
   --pre-build-script "service docker start"
