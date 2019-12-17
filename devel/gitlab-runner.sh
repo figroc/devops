2fe4ca4c93a269cd28417c3459ce59eb90c87f91
Figroc Chen <figroc@gmail.com>
2019-12-17 18:28:50 +0800

    use docker default config

--- devel/gitlab-runner.sh
+++ devel/gitlab-runner.sh
@@ -11,8 +11,6 @@ gitlab-runner register -n --locked false \
   -u ${R_SERVER} -r ${R_TOKENS} \
   --executor docker --docker-privileged \
   --docker-image alpine \
-  --env "DOCKER_DRIVER=overlay2" \
   --env "GIT_SUBMODULE_STRATEGY=recursive" \
-  --docker-volumes "/etc/docker/daemon.json:/etc/docker/daemon.json:ro" \
   --docker-helper-image deepro.io/gitlab/gitlab-runner-helper \
   --pre-build-script "service docker start"
